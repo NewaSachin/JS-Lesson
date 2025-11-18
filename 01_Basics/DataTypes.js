c967156d6b0a730165c6b9015b994f3aa8c13bc7
Sachin Shrestha <117573931+NewaSachin@users.noreply.github.com>
2025-11-18 15:36:20 +0000

    Datatypes

---
+++ 01_Basics/DataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  //Treat all JS code as newer version
+
+// alert(3+3)  //We are using node js, not browser
+
+//Ecma script ->standard for JS
+
+console.log(3+3);
+
+ console.log("Sachin")
+
+let name ="Sachinstha"
+let age =25
+let isLogin = false
+let state;
+
+
+//number =>2 to power 53
+// bigint 
+//string =>""
+//boolean =>true/false
+//null- standalone Value
+// undefined => nodefine value
+// symbol => unique
+
+//object
+
+console.log(typeof undefined)
---
+++ 01_Basics/Variable.js
@@ -0,0 +1,28 @@
+const accountId = 120
+let accountEmail = "Sachin@gmail.com"
+var accountPassword = "123456"
+accountCity = "Biratnagar"
+let accountState;
+
+// accountId = 100  //Not allowed
+
+
+
+
+
+accountEmail ="stha@gmail.com"
+accountPassword = "2345"
+accountCity = "Ktm"
+
+
+console.log(accountId)
+
+/*
+Prefer let-
+because of issue in blocked scope and functional scope
+
+*/ 
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
